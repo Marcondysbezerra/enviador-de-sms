f7c5d7157949da054476d0dbe2dba01cf20d84ea
Marcondys Santos <marcondysbezerra@gmail.com>
2023-03-17 17:38:56 -0300

    Editado campos com número do remetente e destinatário

--- main.py
+++ main.py
@@ -15,13 +15,12 @@ for mes in lista_meses:
     if (tabela_vendas['Vendas'] > 55000).any():
         vendedor = tabela_vendas.loc[tabela_vendas['Vendas'] > 55000, 'Vendedor'].values[0]
         vendas = tabela_vendas.loc[tabela_vendas['Vendas'] > 55000, 'Vendas'].values[0]
-        print(f'No mês de {mes}, o vendedor {vendedor} bateu a meta de R${vendas}.')
 
         message = client.messages \
                         .create(
                              body=f"Você {vendedor} bateu a meta de vendas {vendas} no mês de {mes} e foi contemplado com R$10.000,00.",
-                             from_='+15673613184',
-                             to='+5570_SUE_NÚMERO'
+                             from_='+15673613184_NÚMERO_DA_PLATAFORMA_TWILIO',
+                             to='+5570_NÚMERO_DO_DESTINATÁRIO'
                          )
 
         print(message.sid)
